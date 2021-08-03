d68999cdd22930353be377093a98891e2a0a18e8
Dilshan <dilshantanki@gmail.com>
2021-08-03 16:09:20 +0530

    Completed app

---
+++ lib/utils/ai_util.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+mixin AIColor {
+  static Color primaryColor1 = Vx.hexToColor("#c33764");
+  static Color primaryColor2 = Vx.hexToColor("#1d2671");
+}
